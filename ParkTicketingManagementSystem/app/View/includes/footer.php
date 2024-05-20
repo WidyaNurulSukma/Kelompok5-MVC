754e08ee152c87c638007bb37ff27ce0d84ba3e9
Ahmad Arif Nuhuda <mbkm24gn0013@mhs.usk.ac.id>
2024-05-21 00:11:04 +0700

    first init git

--- ParkTicketingManagementSystem/app/View/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-            <div class="footer-area">
-                <p>Park Ticketing Management System @ 2019. All right reserved</p>
-            </div>
-        </footer>
--- ParkTicketingManagementSystem/app/helpers/url_helper.php
+++
@@ -1,5 +0,0 @@
-function base_url($path = '') {
-  // Replace with your actual domain name when deploying
-  $base_url = 'http://localhost/your_project/';
-  return $base_url . $path;
-}
